2cc4f382368ab3d158a84b9712cc66a3a069fcdb
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-04-10 16:56:24 +0200

    added TrainTicket Class

---
+++ src/main/java/Calculator/TrainTicket.java
@@ -0,0 +1,66 @@
+package Calculator;
+
+import java.math.BigDecimal;
+
+public class TrainTicket {
+    //ATTRIBUTI
+    final BigDecimal PRICE_PER_KM = new BigDecimal("0.21");
+    final int UNDER_AGE = 18;
+    final int OVER_AGE = 65;
+    final int OVER_DISCOUNT = 40;
+    final int UNDER_DISCOUNT = 20;
+    int kilometresTravel;
+    int age;
+    //COSTRUTTORE
+    public TrainTicket(int kilometresTravel, int age)
+            throws IllegalArgumentException{
+        validatePositiveIntegers(kilometresTravel);
+        validatePositiveIntegers(age);
+        if(age > 120){
+            throw new IllegalArgumentException("The field age is too large");
+        }
+        this.kilometresTravel = kilometresTravel;
+        this.age = age;
+    }
+    //METODI
+    //validazioni
+    private void validatePositiveIntegers(int n)
+            throws IllegalArgumentException {
+        if (n < 1) {
+            throw new IllegalArgumentException("Il valore deve essere un numero intero positivo finito.");
+        }
+    }
+    //setter e getter
+    public int getKilometresTravel() {
+        return kilometresTravel;
+    }
+    public void setKilometresTravel(int kilometresTravel) {
+        this.kilometresTravel = kilometresTravel;
+    }
+    public BigDecimal getPRICE_PER_KM() {
+        return PRICE_PER_KM;
+    }
+    public int getUNDER_AGE() {
+        return UNDER_AGE;
+    }
+    public int getOVER_AGE() {
+        return OVER_AGE;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    //altri metodi
+    public BigDecimal CalculateDiscount(){
+        BigDecimal price = PRICE_PER_KM.multiply(new BigDecimal(kilometresTravel));
+        if(age >= OVER_AGE){
+            return price.multiply(BigDecimal.ONE.subtract(BigDecimal.valueOf(OVER_DISCOUNT).divide(BigDecimal.valueOf(100)))).setScale(2);
+        }else if(age <= UNDER_AGE){
+            return price.multiply(BigDecimal.ONE.subtract(BigDecimal.valueOf(UNDER_DISCOUNT).divide(BigDecimal.valueOf(100)))).setScale(2);
+        }else{
+            return price.setScale(2);
+        }
+    }
+}
