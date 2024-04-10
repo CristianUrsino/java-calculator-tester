4de56ce648fad4f62c6e6193d5fe3eed32f9684c
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-04-10 14:35:02 +0200

    added Calculator class

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -1,5 +1,44 @@
 package Calculator;
 
 public class Calculator {
-
+    //ATTRIBUTI
+    //COSTRUTTORE
+    private Calculator(){};
+    //METODI
+    //validazioni
+    static private void validateFloat(float f)
+            throws IllegalArgumentException{
+        if(f < 0 || Float.isNaN(f)){
+            throw new IllegalArgumentException("The values must be a positive numbers");
+        }
+        float maxFloat = Float.MAX_VALUE;
+        if(f > maxFloat){
+            throw new IllegalArgumentException("The value is too large");
+        }
+    }
+    static public float add(float num1, float num2)
+            throws IllegalArgumentException{
+        validateFloat(num1);
+        validateFloat(num2);
+        return num1 + num2;
+    }
+    static public float subtract(float num1, float num2)
+            throws IllegalArgumentException{
+        validateFloat(num1);
+        validateFloat(num2);
+        return num1 - num2;
+    }static public float divide(float num1, float num2)
+            throws IllegalArgumentException, ArithmeticException{
+        validateFloat(num1);
+        validateFloat(num2);
+        if(num2 == 0){
+            throw new ArithmeticException("division by 0 is impossible");
+        }
+        return num1 / num2;
+    }static public float multiply(float num1, float num2)
+            throws IllegalArgumentException{
+        validateFloat(num1);
+        validateFloat(num2);
+        return num1 * num2;
+    }
 }
